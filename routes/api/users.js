dd41b766046e2654f6d26aff5fd191ce5e2ec2c6
Curtis Hlongwane <curtishlongwane@gmail.com>
2020-07-21 07:00:53 +0200

    Register and Sign in Server side validation Implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateSigninInput  = require('../../validation/signin')
 
 
 // Load Models
@@ -64,6 +65,12 @@ router.post('/register', (req, res) => {
 // @desc	Signin a user
 // @access	Public
 router.post('/signin', (req, res) => {
+	const {errors, isValid} = validateSigninInput(req.body);
+
+	// Check validation
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
 	const {email, password} = req.body;
 
 	// Find user by email
@@ -71,7 +78,8 @@ router.post('/signin', (req, res) => {
 	.then(user => {
 		// Check for user
 		if(!user) {
-			return res.status(404).json({email: 'User not found'});
+			errors.email = "User not found"
+			return res.status(404).json(errors);
 		}
 
 		// Check password
@@ -93,7 +101,8 @@ router.post('/signin', (req, res) => {
 						});
 					});
 			} else{
-				return res.status(400).json({password: 'Password incorrect'});
+				errors.password = 'Password incorrect'
+				return res.status(400).json(errors);
 			}
 		})
 	});
