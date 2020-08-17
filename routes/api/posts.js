5e1d1c98639b1d83c129c39095647b5804c93b5d
Curtis Hlongwane <curtishlongwane@gmail.com>
2020-08-17 21:16:14 +0200

    Post creation and fetching implemented

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,25 @@ const  validatePostInput = require('../../validation/post');
 // @access	Public
 router.get('/test', (req, res) => res.json('posts works'));
 
+// @router 	Get api/posts
+// @desc	Fetch post
+// @access	Public
+router.get('/', (req, res) => {
+	Post.find()
+	.sort({date: -1})
+	.then(posts => res.json(posts))
+	.catch(err => res.status(404).json({NoPostsFound: 'No post found'}));
+});
+
+// @router 	Get api/posts/:id
+// @desc	Fetch post
+// @access	Public
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+	.then(posts => res.json(posts))
+	.catch(err => res.status(404).json({NoPostFound: 'No post found with that ID'}));
+});
+
 // @router 	POST api/posts
 // @desc	Create post
 // @access	Private
@@ -26,12 +45,17 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 
 	const newPost = new Post({
 		text: req.body.text,
-		name: req.boody.name,
-		avatar: req.body.name,
+		name: req.body.name,
+		avatar: req.body.avatar,
 		user: req.user.id
 	});
 
 	newPost.save().then(post => res.json(post))
 });
+
+// @router 	POST api/posts
+// @desc	Delete post from profile
+// @access	Private
+//router.delete('/experience/:exp_id', passport.authenticate('jwt', {session: false}), (req, res) => {
 
 module.exports = router;
